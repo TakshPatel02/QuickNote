1ca95ec709610c56d6228595c4fe17832394ddff
Taksh Patel <takshpatel022@gmail.com>
2025-12-15 09:51:33 +0530

    Fix import statement for WishList component and standardize formatting in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
-import Notes from './Pages/Notes'
-import Wishlist from './Pages/Wishlist'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Notes from "./Pages/Notes";
+import WishList from "./Pages/WishList";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/notes' element={<Notes />} />
-        <Route path='/wishlist' element={<Wishlist />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/wishlist" element={<WishList />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
